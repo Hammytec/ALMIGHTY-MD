ed8d593c6dd4281fc20527c84ca449e547d51498
Hammytec <hammytech2@gmail.com>
2024-07-21 19:35:03 +0300

    𝐀𝐋𝐌𝐈𝐆𝐇𝐓𝐘-𝐌𝐃

--- Almighty.js
+++ Almighty.js
@@ -1,1 +1,17 @@
 
+const bot = require(__dirname + '/lib/Almighty')
+const { VERSION } = require(__dirname + '/config')
+
+const start = async () => {
+    Debug.info(`Starting Almighty - ${VERSION}`)
+  try {
+    await bot.init()
+    //bot.logger.info('⏳ Database Syncing!')
+    await bot.DATABASE.sync()
+    await bot.connect()
+  } catch (error) {
+    Debug.error(error);
+    start();
+  }
+}
+start();
